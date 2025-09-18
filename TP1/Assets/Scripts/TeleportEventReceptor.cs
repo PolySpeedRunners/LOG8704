64224fb57c3dc3eda52fc12bd2c91c2fc445c352
lfsta <algorix1217@gmail.com>
2025-09-18 19:03:12 -0400

    correct detection of teleport

---
+++ TP1/Assets/Scripts/TeleportEventReceptor.cs
@@ -0,0 +1,28 @@
+using Oculus.Interaction.Locomotion;
+using System;
+using UnityEngine;
+
+public class TeleportEventReceptor : MonoBehaviour, ILocomotionEventHandler
+{
+    public event Action<LocomotionEvent, Pose> WhenLocomotionEventHandled;
+
+    public void HandleLocomotionEvent(LocomotionEvent locomotionEvent)
+    {
+        if (locomotionEvent.Translation == LocomotionEvent.TranslationType.Absolute)
+        {
+            Debug.Log("We received request to teleport");
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
