47e451a96105cf7b7a80c8737f76f7c73a124a8c
LFSTA <algorix1217@gmail.com>
2025-11-04 00:15:36 -0500

    snap works pour guirlande et bas de noel

---
+++ TP2/Assets/Scripts/StickToWall.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System.Collections.Generic;
+
+public class StickToWall : MonoBehaviour
+{
+    ARRaycastManager raycastManager;
+    Rigidbody rb;
+    [SerializeField]
+    bool isGlued = false;
+
+    void Start()
+    {
+        raycastManager = FindAnyObjectByType<ARRaycastManager>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (isGlued) return; 
+
+        if (raycastManager == null) return;
+
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        // ray depuis l'objfet vers l'avant
+        if (raycastManager.Raycast(new Ray(transform.position, transform.forward), hits, TrackableType.Planes))
+        {
+            var plane = hits[0];
+
+            Vector3 normal = plane.pose.up;
+
+            bool isWall = Mathf.Abs(normal.y) < 0.3f;
+
+            // Si c'est un mur, reste là
+            if (isWall)
+            {
+                Vector3 wallNormal = normal;
+                Quaternion look = Quaternion.LookRotation(-wallNormal, Vector3.up);
+                transform.rotation = look * Quaternion.Euler(0, 90, 0);
+
+                rb.useGravity = false;
+                rb.isKinematic = true;
+                isGlued = true;
+            }
+        }
+    }
+}
+
