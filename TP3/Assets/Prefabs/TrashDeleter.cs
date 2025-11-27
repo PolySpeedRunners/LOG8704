3c016f7554db7fa9cd7a434089f39d17fe7b8837
Primexus <pierre.tran@ymail.com>
2025-11-26 19:57:21 -0500

    Update script

--- TP3/Assets/Prefabs/TrashDeleter.cs
+++ TP3/Assets/Prefabs/TrashDeleter.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class TrashDeleter : MonoBehaviour
 {
-    public string requiredTag = "Trashable";
-
     private void OnTriggerEnter(Collider other)
     {
-
         Rigidbody rb = other.attachedRigidbody;
         if (rb == null) return;
 
         Transform root = rb.transform;
 
-        if (!root.CompareTag(requiredTag)) return;
+        ChemicalContainer container = root.GetComponent<ChemicalContainer>();
+        if (container == null)
+        {
+            return;
+        }
 
-        Debug.Log($"[TrashDeleter] DELETING: {root.name}");
         Destroy(root.gameObject);
     }
 }
