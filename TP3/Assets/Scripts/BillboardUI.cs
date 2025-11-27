65a886c135f05b233a4bfeaf729baaf71a9a0d1f
LFSTA <algorix1217@gmail.com>
2025-11-26 19:37:38 -0500

    trying to fix the billboard

--- TP3/Assets/Scripts/BillboardUI.cs
+++ TP3/Assets/Scripts/BillboardUI.cs
@@ -20,20 +20,32 @@ public class BillboardUI : MonoBehaviour
     }
     public void Refresh()
     {
+        if (objectiveText == null)
+        {
+            Debug.LogWarning("BillboardUI: TMP_Text reference is missing!");
+            return;
+        }
+
+
         StringBuilder sb = new StringBuilder();
 
         foreach (var obj in QuestObjectiveManager.Instance.objectives)
         {
-           // sb.Append(obj.chemA + " + " + obj.chemB + " -> " + obj.result);
+            // Sécurité
+            if (obj.recipe == null)
+            {
+                sb.Append("<color=red>Invalid recipe</color>\n");
+                continue;
+            }
+
+            string recipeName = obj.recipe.name != null ? obj.recipe.name : "Reaction";
 
             if (obj.completed)
-                sb.Append("   <color=green>POG</color>");
+                sb.Append($"<color=green>[ Fait ]</color> {recipeName}");
             else
-                sb.Append("   <color=red>TODO</color>");
+                sb.Append($"<color=red>[ À Faire ]</color> {recipeName}");
 
             sb.Append("\n");
         }
-
-        objectiveText.text = sb.ToString();
     }
 }
