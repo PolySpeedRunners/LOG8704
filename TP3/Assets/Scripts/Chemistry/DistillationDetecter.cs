6b100a63e075f269e9d86cdf7c6161e0c752922f
Primexus <pierre.tran@ymail.com>
2025-11-26 22:40:10 -0500

    Add distillation changes

---
+++ TP3/Assets/Scripts/Chemistry/DistillationDetecter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DistillationDetector : MonoBehaviour
+{
+    public DistillationSystem distillationSystem;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var container = other.GetComponentInParent<ChemicalContainer>();
+        if (container != null)
+        {
+            distillationSystem.AttachContainer(container);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var container = other.GetComponentInParent<ChemicalContainer>();
+        if (container != null)
+        {
+            distillationSystem.DetachContainer(container);
+        }
+    }
+}
